b6a18b11454cec1e393281741cb2da7c81bbcdd8
enlighter <mandal.sushovan92@gmail.com>
2015-05-09 20:53:10 +0530

    second version of toggle code doesn't work on os.system

--- git bash customizations/toggleGitProxy.py
+++ git bash customizations/toggleGitProxy.py
@@ -1,13 +1,32 @@
 #!/bin/python3
 
-from io import StringIO
+# from io import StringIO
 import os
-import sys
+# import sys
 
+# old_stdout = sys.stdout
+# sys.stdout = my_stdout = StringIO()
+# os.system("git config --list")
+# gitConfig = my_stdout.getvalue()
+# sys.stdout = old_stdout
+# print(type(gitConfig))
+# print("Screen check")
+
+import sys
+from io import TextIOWrapper, BytesIO
+
+# setup the environment
 old_stdout = sys.stdout
-sys.stdout = my_stdout = StringIO()
+sys.stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)
+
+# do something that writes to stdout or stdout.buffer
 os.system("git config --list")
-gitConfig = my_stdout.getvalue()
+
+# get output
+sys.stdout.seek(0)      # jump to the start
+out = sys.stdout.read() # read output
+
+# restore stdout
+sys.stdout.close()
 sys.stdout = old_stdout
-print(type(gitConfig))
-print("Screen check")
+print(out)
