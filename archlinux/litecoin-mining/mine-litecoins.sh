23c68201e928186f6e3c6704b117ac05a3580f86
enlighter <mandal.sushovan92@gmail.com>
2015-06-21 15:17:40 +0530

    optimize mining script based on mining experiment

--- archlinux/litecoin-mining/mine-litecoins.sh
+++ archlinux/litecoin-mining/mine-litecoins.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD=libcurl.so.3 ./cgminer --scrypt -o stratum+tcp://global.wemineltc.com:3335 -u enlighter92.sys1 -p 9 --thread-concurrency 6192 -I 9
+LD_PRELOAD=libcurl.so.3 ./cgminer --scrypt -o stratum+tcp://global.wemineltc.com:3333 -u enlighter92.sys1 -p 9 -I 9 -d 0
