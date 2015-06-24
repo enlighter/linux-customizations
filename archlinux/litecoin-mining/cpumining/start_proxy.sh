b919cfa01219bae9fb1e90b82b038f5cd06803b7
enlighter <mandal.sushovan92@gmail.com>
2015-06-24 19:17:35 +0530

    optimize cpu-mining scripts

--- archlinux/litecoin-mining/cpumining/start_proxy.sh
+++ archlinux/litecoin-mining/cpumining/start_proxy.sh
@@ -1,1 +1,1 @@
-./mining_proxy.py -pa scrypt -o global.wemineltc.com -p 3333
+./mining_proxy.py -pa scrypt -o freedom.wemineltc.com -p 3339
--- archlinux/litecoin-mining/cpumining/start_proxy_insti.sh
+++ archlinux/litecoin-mining/cpumining/start_proxy_insti.sh
@@ -1,1 +1,1 @@
-./mining_proxy.py -pa scrypt --socks 127.0.0.1:9150 -o global.wemineltc.com -p 80
+./mining_proxy.py -pa scrypt --socks 127.0.0.1:9150 -o freedom.wemineltc.com -p 3339
