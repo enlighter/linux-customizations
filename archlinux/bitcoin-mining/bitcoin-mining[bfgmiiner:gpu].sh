60e3a6e5b8f339ec7998236b08001c4f9acad409
enlighter <mandal.sushovan92@gmail.com>
2015-07-01 18:53:49 +0530

    optimize bfgminer bitcoin script

--- archlinux/bitcoin-mining/bitcoin-mining[bfgmiiner:gpu].sh
+++ archlinux/bitcoin-mining/bitcoin-mining[bfgmiiner:gpu].sh
@@ -1,1 +1,2 @@
- bfgminer --cpu-threads 3 -S opencl:auto --socks-proxy 127.0.0.1:9150 --userpass enlighter.worker1 --url stratum+tcp://us-east.stratum.bitcoin.cz:3333
+ bfgminer --cpu-threads 3 -S opencl:auto --auto-gpu --temp-cutoff 115 --socks-proxy 127.0.0.1:9150 --userpass enlighter.mobile003 \
+--url stratum+tcp://us-east.stratum.bitcoin.cz:3333
