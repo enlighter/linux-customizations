529fe7fa65429bbeb3fcfc664110e8f7995a5a0c
enlighter <mandal.sushovan92@gmail.com>
2016-05-02 20:14:46 +0530

    update my git setup script

--- setup-my-git.sh
+++ setup-my-git.sh
@@ -5,3 +5,4 @@ git config --global core.editor nano
 git config --global push.default upstream
 git config --global merge.conflictstyle diff3
 git config --global url.https://github.com/.insteadOf git://github.com/
+git config --global credential.helper "cache --timeout=3600"
