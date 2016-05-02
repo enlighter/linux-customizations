15526234283acd43952bcb0dcf1bc52539d9c348
enlighter <mandal.sushovan92@gmail.com>
2016-05-02 20:02:29 +0530

    create first time git setup script for me

---
+++ setup-my-git.sh
@@ -0,0 +1,7 @@
+git config --global user.name "enlighter"
+git config --global user.email mandal.sushovan92@gmail.com
+git config --global color.ui auto
+git config --global core.editor nano
+git config --global push.default upstream
+git config --global merge.conflictstyle diff3
+git config --global url.https://github.com/.insteadOf git://github.com/
